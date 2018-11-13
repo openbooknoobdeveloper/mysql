72f88011e9d26de3a1ac5250b3a8390d63a5f1b9
openbooknoobdeveloper <38215936+openbooknoobdeveloper@users.noreply.github.com>
2018-11-13 19:31:41 -0200

    Update name.js

--- name.js
+++ name.js
@@ -1,2 +1,9 @@
 INSERT INTO `members` (`activated`) VALUES (1);
 UPDATE members SET activated='1' WHERE email='admin@example.com';
+
+
+
+share on ubuntu
+sudo mount -t vboxsf FOLDERNAME /PATH/OF/FOLDER
+
+
