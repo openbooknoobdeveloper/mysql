3a0236296c2b2cfc5987e147ef09816e20db99e4
openbooknoobdeveloper <38215936+openbooknoobdeveloper@users.noreply.github.com>
2018-11-13 18:55:55 -0300

    Update name.js

--- name.js
+++ name.js
@@ -6,4 +6,21 @@ UPDATE members SET activated='1' WHERE email='admin@example.com';
 share on ubuntu
 sudo mount -t vboxsf FOLDERNAME /PATH/OF/FOLDER
 
+sudo mount -t vboxsf folder-name-on-windows /PATH/OF/FOLDER-ubuntu-u-created
 
+
+
+How to show only hidden files in Terminal?
+https://askubuntu.com/questions/468901/how-to-show-only-hidden-files-in-terminal
+ls -ld .?* 
+  
+  
+  
+cp -R path_to_source path_to_destination/
+  
+  
+                         copy                                   /move
+  sudo cp -r /home/Username/Desktop/myNewTheme /usr/share/kde4/apps/ksplash/Themes/
+  
+  
+    sudo cp -r /home/ubuntu1604/testnet3 /home/deploy/.bitcoin
