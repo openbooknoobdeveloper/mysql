ae4c7c13e4f784c9c22bdd0ca3a3c114ec40c93d
openbooknoobdeveloper <38215936+openbooknoobdeveloper@users.noreply.github.com>
2018-11-20 15:58:38 -0300

    Update rubykube.js

--- rubykube.js
+++ rubykube.js
@@ -1,1 +1,33 @@
 
+
+probably this one i think........
+
+app/peatio/config/templates/seed/blockchains.yml.erb
+
+
+?
+app/peatio/spec/factories/blockchains.rb
+
+
+Copy /home/app/config/templates/management_api_v1.yml.erb -> /home/app/config/management_api_v1.yml
+Copy /home/app/config/templates/application.yml.erb -> /home/app/config/application.yml
+Copy /home/app/config/templates/seed/currencies.yml.erb -> /home/app/config/seed/currencies.yml
+Copy /home/app/config/templates/seed/blockchains.yml.erb -> /home/app/config/seed/blockchains.yml
+Copy /home/app/config/templates/seed/wallets.yml.erb -> /home/app/config/seed/wallets.yml
+Copy /home/app/config/templates/seed/markets.yml.erb -> /home/app/config/seed/markets.yml
+Copy /home/app/config/templates/plugins.yml.erb -> /home/app/config/plugins.yml
+
+
+sudo make down.
+
+
+config/peatio/seed/blockchains.yml
+config/peatio-seed-btc/blockchains.yml
+
+
+Link /opt/peatio/config/seed/wallets.yml -> /home/app/config/seed/wallets.yml
+Link /opt/peatio/config/seed/currencies.yml -> /home/app/config/seed/currencies.yml
+Link /opt/peatio/config/seed/blockchains.yml -> /home/app/config/seed/blockchains.yml
+Link /opt/peatio/config/seed/markets.yml -> /home/app/config/seed/markets.yml
+Link /opt/peatio/config/management_api_v1.yml -> /home/app/config/management_api_v1.yml
+Link /opt/peatio/config/plugins.yml -> /home/app/config/plugins.yml
