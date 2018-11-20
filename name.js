037455619c9f03b912338c3f6846178afec2c81c
openbooknoobdeveloper <38215936+openbooknoobdeveloper@users.noreply.github.com>
2018-11-20 18:02:38 -0300

    Update name.js

--- name.js
+++ name.js
@@ -43,6 +43,10 @@ install docker compose
 https://docs.docker.com/compose/install/#install-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+
 install docker:
 curl -sSL https://get.docker.com/ | sudo bash
 
